b09677aa78164833216e8044e55b97f10364d5dd
basic02 <xieminling@gmail.com>
2024-09-05 23:59:14 +0800

    Remote CreateSQLTokenStoreTablesCommand & UpgradeSQLTokenStoreTablesCommand

--- cloudera/hdfs-rbf-csd/src/main/scripts/control.sh
+++ cloudera/hdfs-rbf-csd/src/main/scripts/control.sh
@@ -5,12 +5,6 @@ case $1 in
   (start_router)
     export HDFS_RBF_ROLE_TYPE="router"
     ;;
-  (create_sql_token_store_tables)
-    export HDFS_RBF_ROLE_TYPE="command"
-    ;;
-  (upgrade_sql_token_store_tables)
-    export HDFS_RBF_ROLE_TYPE="command"
-    ;;
   (*)
     echo "Don't understand [$1]"
     exit 1
@@ -23,12 +17,6 @@ echo "Running HDFS RBF command: $1"
 case $1 in
   (start_router)
     start_router
-    ;;
-  (create_sql_token_store_tables)
-    create_sql_token_store_tables
-    ;;
-  (upgrade_sql_token_store_tables)
-    upgrade_sql_token_store_tables
     ;;
   (*)
     echo "Don't understand [$1]"
